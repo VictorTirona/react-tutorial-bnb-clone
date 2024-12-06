63e26e550e627ddb109885d5781f02e30f816387
Victor Tirona <vlbtirona@gmail.com>
2024-12-06 16:28:43 +0800

    I wanna work on my iPad

--- src/components/StateExample.js
+++ src/components/StateExample.js
@@ -1,41 +1,17 @@
 import React from "react" //you can also destructure useState to make your code cleaner
 import Star from "./Star.js"
 
-/*State Example #9: BOXES CHALLENGE Part 1
+/*State Example #9: using short circuit evaluation to show or not show something and conditional ternary operators
+    
+STOPED AT: 7:48:52
 */
 export default function StateExample() {
-    const [contact, setContact] = React.useState({
-        firstName: "John",
-        lastName: "Doe",
-        phone: "+1 (719) 555-1212",
-        email: "itsmyrealname@example.com",
-        isFavorite: true
-    })
-
-    function toggleFavorite() {
-        setContact(function (oldStateObj) {
-            return ({
-                ...oldStateObj,
-                isFavorite: !oldStateObj.isFavorite
-            }
-            )
-        })
-    }
+    const [messages, setMessages] = React.useState(["a","b"])
+    const [isShown, setIsShown] = React.useState((messages.length > 0) ? true : false)
 
     return (
-        <main>
-            <article className="card">
-                <div className="card--info">
-                    <img src={'./ramen.png'} width='100px' />
-                    <Star
-                        isFilled={contact.isFavorite} 
-                        handleClick={toggleFavorite} //we are passing the toggleFavorite function as a PROP. Then it'll get used within the child component to update the state. handleClick is the best practice name in order to make it obvious that this is a PROP and not an event listener
-                    />
-                    <h2>{`${contact.firstName} ${contact.lastName}`}</h2>
-                    <p>{`${contact.phone}`}</p>
-                    <p>{`${contact.email}`}</p>
-                </div>
-            </article>
-        </main>
+        <div>
+            <h1>{isShown ? (`You have ${messages.length} unread ${messages.length===1 ? "message":"message(s)"}!`): "You're all caught up!"}</h1>
+        </div>
     )
 }
